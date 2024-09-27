407a4b24e13896ec03464ebc7d0bcc9097c22e07
Chief Marlin <96321026+chiefMarlin@users.noreply.github.com>
2024-09-27 17:43:37 -0400

    Update testValidity.sh

--- testValidity.sh
+++ testValidity.sh
@@ -9,6 +9,10 @@ dd if=/dev/urandom of=randomData bs=1M count=1000 2>/dev/null || fail "Failed to
 CKEY=test go run chap.go e < randomData > encryptedData || fail "Failed to encrypt data"
 CKEY=test go run chap.go d < encryptedData > decryptedData || fail "Failed to decrypt data"
 
+# Test pipe to make sure no decryption errors occur
+dd if=/dev/urandom bs=1M count=1000 | CKEY=test go run chap.go e | CKEY=test go run chap.go d > /dev/null
+
+
 # Check if on macOS, use md5 instead of md5sum
 if [ "$(uname)" == "Darwin" ]; then
     ORIGINAL_MD5=$(md5 -q randomData)
